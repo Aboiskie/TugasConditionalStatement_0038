20125d573920430c060cf650827eba5f02afcbf3
Alby Anbiya_038 <anbiyaalby0@gmail.com>
2026-03-09 23:13:59 +0700

    menampilkan hasil perhitungan BMI dan status

--- BIM.cpp
+++ BIM.cpp
@@ -22,3 +22,21 @@ string statusBMI(double bmi){
     else
         return "Obesitas";
 }
+int main() {
+
+    double berat, tinggi;
+
+    cout << "--- Selamat Datang Mahasiswa Kelas A 2025 ---" << endl;
+
+    inputData(berat, tinggi);
+
+    double bmi = hitungBMI(berat, tinggi);
+    string status = statusBMI(bmi);
+
+    cout << endl;
+    cout << "--- Hasil ---" << endl;
+    cout << "BMI Anda : " << bmi << endl;
+    cout << "Status   : " << status << endl;
+
+    return 0;
+}
